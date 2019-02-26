bc7cfbb12a5701266e51c09718b4502634bf5e25
nstfk <nohaseddik@hotmail.com>
2019-02-26 23:27:21 +0200

    Update elmo2.py

--- examples/elmo2.py
+++ examples/elmo2.py
@@ -38,7 +38,7 @@ params_senteval = {'task_path': PATH_TO_DATA, 'usepytorch': False, 'kfold': 10}
 #weight_file = 'https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/contributed/pubmed/elmo_2x4096_512_2048cnn_2xhighway_weights_PubMed_only.hdf5'
 
 
-if (len(sys.argv)==4)
+if (len(sys.argv)==4):
 	elmo_encoder = ElmoEmbedder(sys.argv[2],sys.argv[3],cuda_device=0)
 else:
 	elmo_encoder = ElmoEmbedder(cuda_device=0)
