0a1814799187ead0331b6dc892a6b4afe1c9e793
nstfk <nohaseddik@hotmail.com>
2019-03-04 20:16:02 +0200

    Update rqe.py

--- senteval/rqe.py
+++ senteval/rqe.py
@@ -104,7 +104,7 @@ class RQEEval(object):
 
         devacc, testacc, yhat = clf.run()
         testf1 = round(100*f1_score(testY, yhat), 2)
-        logging.debug('Dev acc : {0} Test acc {1}; Test F1 {2} for MRPC.\n'
+        logging.debug('Dev acc : {0} Test acc {1}; Test F1 {2} for RQE.\n'
                       .format(devacc, testacc, testf1))
         return {'devacc': devacc, 'acc': testacc, 'f1': testf1,
                 'ndev': len(trainA), 'ntest': len(testA)}
