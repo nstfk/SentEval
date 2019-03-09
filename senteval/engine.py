66adcb7199228ae631e85d9af5a4a2d8e14afc28
nstfk <nohaseddik@hotmail.com>
2019-03-09 14:40:40 +0200

    Update engine.py

--- senteval/engine.py
+++ senteval/engine.py
@@ -49,7 +49,7 @@ class SE(object):
         self.batcher = batcher
         self.prepare = prepare if prepare else lambda x, y: None
 
-        self.list_tasks = ['bioC','CHEMPROT','PICO','PUBMED20K','MEDNLI', 'RQE','STSBenchmark','ClinicalSTS','ClinicalSTS2','BIOSSES']
+        self.list_tasks = ['BIOC','CHEMPROT','PICO','PUBMED20K','MEDNLI', 'RQE','STSBenchmark','ClinicalSTS','ClinicalSTS2','BIOSSES']
                        
 
     def eval(self, name):
