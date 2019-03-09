5626f4c1e6ca9bc4891d901dc0437997065344b6
nstfk <nohaseddik@hotmail.com>
2019-03-09 14:21:10 +0200

    Update and rename biocontradiction.py to bioc.py

--- senteval/bioc.py
+++ senteval/bioc.py
@@ -18,7 +18,7 @@ from senteval.tools.validation import InnerKFoldClassifier
 from sklearn.metrics import f1_score
 
 
-class BioC(object):
+class BioCEval(object):
     def __init__(self, task_path, seed=1111):
         logging.info('***** Transfer task : BioContradictin *****\n\n')
         self.seed = seed
